84660ff008d34cba994063716df206ac9a634523
Gladilov, Gleb <gleb.gladilov@intel.com>
2020-12-23 20:36:08 +0300

    [IE][VPU][nGraph]: Introduces StaticShapeLoop
    StaticShapeLoop is a class derived from Loop
    to override validate_and_infer_types method
    in order to propagate upper-bounds through the
    function instead of dynamic tensors.
    
    It uses maximum_value estimation to get
    upper-bound for iterations count in case if
    it is dynamic.
    
    Signed-off-by: Gladilov, Gleb <gleb.gladilov@intel.com>

---
+++ inference-engine/src/vpu/common/include/vpu/ngraph/operations/static_shape_loop.hpp
@@ -0,0 +1,22 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <ngraph/opsets/opset5.hpp>
+
+namespace ngraph { namespace vpu { namespace op {
+
+class StaticShapeLoop : public ngraph::opset5::Loop {
+public:
+    static constexpr NodeTypeInfo type_info{"StaticShapeLoop", 5};
+    const NodeTypeInfo& get_type_info() const override { return type_info; }
+
+    explicit StaticShapeLoop(const Loop& loop);
+    void validate_and_infer_types() override;
+};
+
+}  // namespace op
+}  // namespace vpu
+}  // namespace ngraph

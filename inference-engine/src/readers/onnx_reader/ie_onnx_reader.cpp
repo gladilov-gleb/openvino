c761598801ee110a800061afeb6381ecbe4e1443
Gladilov, Gleb <gleb.gladilov@intel.com>
2021-01-22 21:31:19 +0300

    [IE][VPU]: Fixes input shape to 3x800x10888
    Signed-off-by: Gladilov, Gleb <gleb.gladilov@intel.com>

--- inference-engine/src/readers/onnx_reader/ie_onnx_reader.cpp
+++ inference-engine/src/readers/onnx_reader/ie_onnx_reader.cpp
@@ -62,7 +62,9 @@ bool ONNXReader::supportModel(std::istream& model) const {
 }
 
 CNNNetwork ONNXReader::read(std::istream& model, const std::vector<IExtensionPtr>& exts) const {
-    return CNNNetwork(ngraph::onnx_import::import_onnx_model(model, readPathFromStream(model)), exts);
+    auto f = ngraph::onnx_import::import_onnx_model(model, readPathFromStream(model));
+    f->get_parameters().front()->set_partial_shape({3, 800, 1088});
+    return CNNNetwork(f, exts);
 }
 
 INFERENCE_PLUGIN_API(StatusCode) InferenceEngine::CreateReader(IReader*& reader, ResponseDesc *resp) noexcept {
